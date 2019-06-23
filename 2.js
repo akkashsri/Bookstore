49cb3df23d96dddfffc0ac39d0722d56d57e4268
aakash <akashkumar59200@gmail.com>
2019-06-23 22:37:28 +0530

    first version

---
+++ 2.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+<head>
+<script src="jquery-3.3.1.min.js" ></script>
+<script>
+$(document).ready(function(){
+$("#left").click(function(){alert("hi")})
+
+});
+</script>
+</head>
+<body>
+<div id="left"><p>This one is left aligned</p>
+</div>
+</body>
+</html>
